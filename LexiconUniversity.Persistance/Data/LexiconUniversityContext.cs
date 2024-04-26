d93a5d20f06dacb86d80ee9f52896563e493c80e
Jonathan Krall <jonathan.krall@lexicon.se>
2024-04-26 13:18:05 +0200

    StudentConfiguration

--- LexiconUniversity.Persistance/Data/LexiconUniversityContext.cs
+++ LexiconUniversity.Persistance/Data/LexiconUniversityContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using LexiconUniversity.Core.Entities;
+using LexiconUniversity.Persistance.Configurations;
 
 namespace LexiconUniversity.Persistance.Data
 {
@@ -26,32 +27,8 @@ namespace LexiconUniversity.Persistance.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            //Wrong name
-            //modelBuilder.Entity<Student>().OwnsOne(s => s.Name); 
-
-            modelBuilder.Entity<Student>()
-                .OwnsOne(s => s.Name)
-                .Property(n => n.FirstName)
-                .HasColumnName("FirstName");
-
-            modelBuilder.Entity<Student>()
-                .OwnsOne(s => s.Name)
-                .Property(n => n.LastName)
-                .HasColumnName("LastName");
-
-            //modelBuilder.Entity<Student>().HasOne(s => s.Address)
-            //    .WithOne(a => a.Student)
-            //    .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<Student>()
-                .HasMany(s => s.Courses)
-                .WithMany(c => c.Students)
-                .UsingEntity<Enrollment>(
-                e=> e.HasOne(e=>e.Course).WithMany(c=>c.Enrollments),
-                e =>e.HasOne(e=>e.Student).WithMany(s=>s.Enrollments),
-                e =>e.HasKey(e =>new {e.CourseId, e.StudentId}));
-
-            //modelBuilder.Entity<Enrollment>().HasKey(e => new { e.StudentId, e.CourseId }); 
+            modelBuilder.ApplyConfiguration(new StudentConfigurations()); 
+            
         }
     }
 }
