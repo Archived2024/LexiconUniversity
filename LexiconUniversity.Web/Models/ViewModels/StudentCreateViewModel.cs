bc6d2027af8e0cfe5299d487833ec5481018bbbf
Jonathan Krall <jonathan.krall@lexicon.se>
2024-04-29 13:41:47 +0200

    StreetNr Validation

--- LexiconUniversity.Web/Models/ViewModels/StudentCreateViewModel.cs
+++ LexiconUniversity.Web/Models/ViewModels/StudentCreateViewModel.cs
@@ -1,14 +1,18 @@
-﻿namespace LexiconUniversity.Web.Models.ViewModels
+﻿using LexiconUniversity.Web.Validations;
+
+namespace LexiconUniversity.Web.Models.ViewModels
 {
     public class StudentCreateViewModel
     {
         public string NameFirstName { get; set; }
         public string NameLastName { get; set; }
         public string Email { get; set; }
+        [CheckStreetNr(10)]
         public string AddressStreet { get; set; }
         public string AddressZipCode { get; set; }
         public string AddressCity { get; set; }
 
-        public List<int> SelectedCourses { get; set; } = new List<int>(); 
+        public List<int> SelectedCourses { get; set; } = new List<int>();
+        
     }
 }
