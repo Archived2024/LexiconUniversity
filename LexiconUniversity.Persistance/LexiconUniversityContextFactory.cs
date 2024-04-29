37e20a4631b0acc8e39669bc4c48d0a094d2de9c
Jonathan Krall <jonathan.krall@lexicon.se>
2024-04-29 13:20:08 +0200

    Added Factory and CoursesController

---
+++ LexiconUniversity.Persistance/LexiconUniversityContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using LexiconUniversity.Persistance.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconUniversity.Persistance
+{
+    //internal class LexiconUniversityContextFactory : IDesignTimeDbContextFactory<LexiconUniversityContext>
+    //{
+    //    public LexiconUniversityContext CreateDbContext(string[] args)
+    //    {
+    //        var options = new DbContextOptionsBuilder<LexiconUniversityContext>();
+    //        options.UseSqlServer("Not required for scaffolding"); //Change to actual connection string if needed
+
+    //        return new LexiconUniversityContext(options.Options); 
+    //    }
+    //}
+}
